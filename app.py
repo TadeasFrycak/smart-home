e8fb91c771ee5b047b92ddbfa3b38df63d8d4a12
Tadeáš Fryčák <frycaktadeas@seznam.cz>
2019-08-15 19:17:04 +0200

    feat: v5.4

--- app.py
+++ app.py
@@ -1,56 +1,149 @@
-from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context, request
-from random import random
-from time import sleep
+from flask_socketio import SocketIO, emit
 from threading import Thread, Event
 import subprocess
+import os.path
+import random
+import json
+import glob
+import time
+import os
 
+SEPARATORS = (",", ":")
+IMG_PATH = "static/Img"
+IMG_BCG = "bcg"
+
+CURRENT_FOLDER = os.getcwd()
+CURRENT_FILE = __file__
+
+current_version = CURRENT_FOLDER.split("IoT")
+current_version = current_version[len(current_version)-1].strip()
+print("------------------------------------------------------------")
+print("Smart Home - App")
+print("Version: " + current_version)
+print("Authors: Filip Szkandera, Tadeáš Fryčák")
+print("IP: 127.0.0.1")
+print("Port: 5000")
+print("Working directory: ")
+print("  - Path: " + CURRENT_FOLDER)
+print("  - Created: %s" % time.ctime(os.path.getctime(CURRENT_FOLDER)))
+print("  - Last modified: %s" % time.ctime(os.path.getmtime(CURRENT_FOLDER)))
+print("Flask Python file:")
+print("  - Path: " + CURRENT_FILE)
+print("  - Created: %s" % time.ctime(os.path.getctime(CURRENT_FILE)))
+print("  - Last modified: %s" % time.ctime(os.path.getmtime(CURRENT_FILE)))
+print("------------------------------------------------------------")
+
+# Create app
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 thread = Thread()
 thread_stop_event = Event()
 
-class RandomThread(Thread):
-    def __init__(self):
-        self.delay = 1
-        super(RandomThread, self).__init__()
 
-    def randomNumberGenerator(self):
+class AsynCommunication(Thread):
+    """
+    AsynCommunication class
+    """
+    
+    # Define some constants
+    DELAY = 10
+    NAMESPACE = "/test"
+    NAME = "newstate"
+    
+    def __init__(self):
+        """
+        Init of class AsynCommunication
+        """
+        
+        super(AsynCommunication, self).__init__()
+
+    def test_generator(self):
+        """
+        Send some test data to script
+        :return:
+        """
+        
         while not thread_stop_event.isSet():
-            socketio.emit("newstate", {"mac": "helllo"}, namespace="/test")
-            sleep(self.delay)
+            socketio.emit(self.NAME, {"mac": "helllo"}, namespace=self.NAMESPACE)  # Send message
+            time.sleep(self.DELAY)  # Sleep
 
     def run(self):
-        self.randomNumberGenerator()
+        """
+        Run Asyn Communication
+        :return:
+        """
+        
+        self.test_generator()
 
 
+@app.route("/change_item_state/<data>")
+def change_item_state(data):
+    """
+    Change item state
+    :data: data
+    :return: JSON
+    """
+    
+    return json.dumps({"status": random.choice(["ok", "not ok", "pending"]), "debug_data": json.loads(data)}, separators=SEPARATORS)
 
-# Server diagnostic
-@app.route('/')
+
+@app.route("/get_background_images")
+def get_background_images():
+    """
+    Get background images
+    :return: background images
+    """
+
+    backgrounds = []
+    
+    os.chdir(IMG_PATH)
+
+    for file in glob.glob("*.*"):
+        if IMG_BCG in file:
+            backgrounds.append("../" + IMG_PATH + "/" + file)
+            
+    os.chdir("../../")
+    
+    return json.dumps({"status": "ok", "images": backgrounds}, separators=SEPARATORS)
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    global thread
-    print('Client connected')
-
-    if not thread.isAlive():
-        print("Starting Thread")
-        thread = RandomThread()
-        thread.start()
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-
-@app.route("/post", methods = ["POST"])
-def get_mac():
-    data = request.form["data"]
-    print(data.split("<mac>")[1].split("</mac>")[0])
-    wakeonlan.send_magic_packet(data.split("<mac>")[1].split("</mac>")[0])
+    """
+    Render index.html file
+    :return: template to render
+    """
+    
     return render_template("index.html")
 
-if __name__ == '__main__':
+
+@socketio.on("connect", namespace="/test")
+def client_connect():
+    """
+    Event on user connect
+    :return:
+    """
+
+    print("Client connected")
+    global thread
+
+    if not thread.isAlive():  # When AsynCommunication is not started
+        print("Starting Thread")
+        
+        thread = AsynCommunication()
+        thread.start()  # Start AsynCommunication
+
+
+@socketio.on("disconnect", namespace="/test")
+def client_disconnect():
+    """
+    Event on user discconnect
+    :return:
+    """
+    
+    print("Client disconnected")
+
+if __name__ == "__main__":
     socketio.run(app)
--- static/script.js
+++ static/script.js
@@ -3,30 +3,35 @@
 
 $(document).ready(function(){      
 // Spustí se při načtení stránky
+
+    console.log("+----------------------------------+");
+    console.log("|     IoT Project, version 5.4     |");
+    console.log("|     Last modified: 15.8.2019     |");
+    console.log("|                                  |");
+    console.log("|              © 2019              |");
+    console.log("+----------------------------------+");
+
     // Asynchroní přijem dat
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
     socket.on('newstate', function(msg) {
-        console.log("Received msg: " + msg.mac);    
+        console.log("Received message: ");
+        console.log(msg.mac);    
     });
     
     // Initializace Swiperu 
     var swiper = new Swiper('.swiper-container', {
-        pagination: {
-          el: '.swiper-pagination',
-        },
+        pagination: { el: '.swiper-pagination', },
         threshold: '10'
       });
 
+    // Vypocet pozice pro scroll objekty
     var PredchoziProcenta = 0;    
     var overlayYpos = 210;
     
-    $(document).on('input', '.slider', function() {     
-    //$( ".slider" ).on( "click", function() {            // Spustí se při kliknutí nebo posunutím scroll modulu
-    //$(".slider").on("click"),function(
+    $(document).on('input', '.slider', function() {                     // Spustí se při kliknutí nebo posunutím scroll modulu
         
-        var output = $(this).val();                                     // Načítá hodnotu scroll modulu 
-        //var konstanta = parseFloat(0.476190476190476); 
-        var konstanta = parseFloat(0.47619);                            // Konstanta pro přepočítávání pozice
+        var output = $(this).val();                                     // Načítá okamžitou hodnotu kurzoru
+        var konstanta = parseFloat(0.47619);                            // Konstanta pro přepočítávání pozice na pixely
 
         var procenta = Math.round(output * konstanta / 10) * 10;        // Počítání procent 
         var outputText = "";
@@ -35,7 +40,7 @@ $(document).ready(function(){
         if (procenta == 0)  outputText = "OFF";                         // Rozhodování mezi stavem OFF nebo číselnou hodnotou
         else outputText = procenta + "%"; 
         
-        overlayYpos = Math.abs((output - 210));                     // Počítání relatvní pozice pro .box (overlay)
+        overlayYpos = Math.abs((output - 210));                         // Počítání relatvní pozice pro .box (overlay)
         $('.debug').html(output - 210);
         
         if (procenta != PredchoziProcenta){
@@ -58,11 +63,17 @@ $(document).ready(function(){
         $(this).parent().children(".box").css("top",overlayYpos + "px");
     });
 
+
+
+    /*  -- Generování modulů -- */
+    
     var device_config_array;
+
     // Načítání souborů 
     $.get('/static/device-config.txt', function(device_config_data) {
         $.get('/static/items.html.txt', function(items_data){
             $.get('/static/icons-path.txt', function(icons_path_file) {
+
                 // Generuje pole z konfiguračních složek
                 device_config_array = device_config_data.split('\n');
                 items_data_list = items_data.split('\n');
@@ -74,12 +85,12 @@ $(document).ready(function(){
                     if(device_config_array[i].indexOf("<module>") >= 0 && device_config_array[i].indexOf("</module") >= 0 && device_config_array[i].indexOf(";") == -1)
                     {
                         
-                        var type = device_config_array[i].substring(device_config_array[i].indexOf("<type>") + 6,device_config_array[i].indexOf("</type>"));
-                        var name = device_config_array[i].substring(device_config_array[i].indexOf("<name>") + 6,device_config_array[i].indexOf("</name>"));
-                        var pageForItem = device_config_array[i].substring(device_config_array[i].indexOf("<page-id>") + 9,device_config_array[i].indexOf("</page-id>"));
-                        var iconID = device_config_array[i].substring(device_config_array[i].indexOf("<icon-id>") + 9,device_config_array[i].indexOf("</icon-id>")); 
+                        var type = $.trim(device_config_array[i].split("<type>")[1].split("</type")[0]);;
+                        var name = $.trim(device_config_array[i].split("<name>")[1].split("</name>")[0]);
+                        var pageForItem = $.trim(device_config_array[i].split("<page-id>")[1].split("</page-id>")[0]);
+                        var iconID = $.trim(device_config_array[i].split("<icon-id>")[1].split("</icon-id>")[0]);
 
-                        // Generuje item příslušného typu na příslušnou stránku
+                        // Generuje toggle item
                         if (type == "toggle") 
                         {
                             var prepared_item = items_data_list[1];
@@ -89,7 +100,7 @@ $(document).ready(function(){
                             $("#" + pageForItem).append(prepared_item);  
                         }
 
-                        // Generuje item příslušného typu na příslušnou stránku
+                        // Generuje scroll item
                         if (type == "scroll") 
                         {
                             var prepared_item = items_data_list[3];
@@ -107,67 +118,98 @@ $(document).ready(function(){
 
      
      
-     
+     // Definování CSS barev do proměnných
      var item_unactive_color = 'rgb(206, 206, 206)';
      var item_active_color = 'rgb(255, 255, 255)';
 
-     $.get('/static/wallpapers-paths.txt', function(wallpapers) {
-        wallpper_array = wallpapers.split('\n');
-        var wallpaper_index = Math.floor(Math.random() * wallpper_array.length) + 1
-        console.log(wallpper_array[wallpaper_index]);
+
+    /*  -- Načítání pbrázků na pozadí -- */
+
+    $.get('/get_background_images', function(data) {
+        var wallpper_array = $.parseJSON(data).images;
+        console.log($.parseJSON(data));
+        console.log(wallpper_array);
+        var wallpaper_index = Math.floor(Math.random() * wallpper_array.length)
 
         $('body').css('background-image', 'url("' + wallpper_array[wallpaper_index] + '")');
-     },'text');
-     
+    });
      
 
-    // switch(Math.floor(Math.random() * 8) + 1){
-    //     case 1:
-    //             $('body').css('background-image', 'url("/static/Img/bcgImg1.dms")');
-    //         break;
-    //     case 2:
-    //             $('body').css('background-image', 'url("/static/Img/bckImg2.jpg")');
-    //         break;
-    //     case 3:
-    //             $('body').css('background-image', 'url("/static/Img/bcgImg4.jpg")');
-    //         break;
-    //     case 4:
-    //             $('body').css('background-image', 'url("/static/Img/bcgImg5.jpg")');
-    //         break;
-    //     case 5:
-    //             $('body').css('background-image', 'url("/static/Img/bcgImg6.jpg")');
-    //         break;
-    //     case 6:
-    //             $('body').css('background-image', 'url("/static/Img/bcgImg7.jpg")');
-    //         break;
-    //     case 7:
-    //             $('body').css('background-image', 'url("/static/Img/bcgImg8.jpg")');
-    //         break;
-    //     case 8:
-    //         $('body').css('background-image', 'url("/static/Img/bcgImg9.jpg")');
-    //     break;
-    // }
-
+    /*  -- Detekování stisku toggle itemu -- */
 
     $('body').on('click', '.item-toggle', function() {
-        ChangeItemState(this,2); // DRUHY CISLO MUSI BYT 2 VZDYCKY POKUD NECHCEEM NIC KONKRETNIHO!!!
+        //ChangeItemState(this,2); // DRUHY CISLO MUSI BYT 2 VZDYCKY POKUD NECHCEEM NIC KONKRETNIHO!!!
+
+        var value = 0;
+
+        if($(this).css('background-color')==item_unactive_color)
+        {
+            ChangeItemState(this,100);
+            value = 100;
+        }
+        else if($(this).css('background-color')==item_active_color)
+        {
+            ChangeItemState(this,0);
+            value = 0;
+        }
+
+        $(this).animate({
+            "color": "#ffffff"
+        }, 1500);
+
+        
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        var Data_of_element = $(this).find("module").html();
+        Jasonify = Data_of_element.split(' ');
+        //console.log(Jasonify);
+        Jasonify.forEach(function(entry) {
+            if (entry != ""){
+                entry = entry.replace("<","\"")
+                console.log(entry);
+            }
+        });
+        
+        var element = Data_of_element;
+        //console.log(element);
+        //  This gives you a string representing that element and its content
+        var html = element.oute; 
+        //console.log(html);      
+        //  This gives you a JSON object that you can send with jQuery.ajax's `data`
+        // option, you can rename the property to whatever you want.
+        var data = { html: html }; 
+
+        //  This gives you a string in JSON syntax of the object above that you can 
+        // send with XMLHttpRequest.
+        var json = JSON.stringify(data);
+
+        //console.log(json);
+        
+        $.get('/change_item_state/{"item_id":' + "ID_of_element" + ', "value":' + "value" + '}', function(data) {
+            //console.log("Sending data, synchronized..");
+            //console.log("Response: " + $.parseJSON(data));
+            //  This gives you an HTMLElement object
+
+
+            //console.log($.parseJSON(data))
+        });
 
         // JEN PRO TEST! NASTAVUJI OD KTERE SE MA ZAPNOUT
         //toggleButton(3,1);
-        
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     });
 
 
     function ChangeItemState(button, stav){
-        if(stav == 1 || stav == 2 && $(button).css('background-color')==item_unactive_color)
+
+        if(stav == 100)
         {
-            //console.log(button);
             $(button).css('background-color', item_active_color);
             $(button).children('.item-status').text("ON");
             $(button).children('.item-image').fadeTo("fast",1);
             $(button).children('.item-header').fadeTo("fast",1);
         }
-        else if (stav == 0 || stav == 2 && $(button).css('background-color')==item_active_color){
+        else if (stav == 0){
             $(button).css('background-color', item_unactive_color);
             $(button).children('.item-status').text("OFF");
             $(button).children('.item-image').fadeTo("slow",0.33);
@@ -182,7 +224,7 @@ $(document).ready(function(){
 
             if ($( this ).html() == id){console.log("found: " + id); 
             var itemToToggle = $( this ).parent().parent().parent();
-
+            
             ChangeItemState(itemToToggle,stav);
         }
         });
