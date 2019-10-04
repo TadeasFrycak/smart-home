5753ca8435557e9341e460b4ff6e7c868fe60d17
Tadeáš Fryčák <frycaktadeas@seznam.cz>
2019-10-04 23:18:06 +0200

    feat: v10.1

--- static/main.js
+++ static/main.js
@@ -1,71 +1,52 @@
-$(document).ready(function(){     
+$(document).ready(function(){ 
+  
 
+  /**
+   * 
+   *  Globální promněné
+   * 
+   */
+    
+   // Definování CSS barev do proměnných
+  var item_unactive_color = 'rgb(206, 206, 206)';
+  var item_active_color = 'rgb(255, 10, 255)';
+  
+
+
+  /**
+   * 
+   *  Kód pro initializování objektů
+   * 
+   */
+  
+/*
   $('.slider').each(function(){
     slider = new hx.Slider(this,{max:100});
   });
-  $('.toggle').each(function(){
-    var toggle = new hx.Toggle(this)
-  });
 
-  slider.on('change', function(){
-    console.log("aa");
-  });
-
-  $('.timepicker').timepicker();
-  
-
-    // Definování CSS barev do proměnných
-    var item_unactive_color = 'rgb(206, 206, 206)';
-    var item_active_color = 'rgb(255, 10, 255)';
-
-
-    // Get the modal
-    var modal = document.getElementById("tile-Modal");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
-    $('.tileToggle').each(function(){
-        var $this = $(this);
-        var mc = new Hammer(this);
-        mc.on("tap", function() {
-            $this.parent().toggleClass('tileActive');
-        });
+  $(".slider").each(function() {
+    slider = new hx.Slider(this);
+    slider.change(function(value) {
+        console.log("test");
     });
-
-    $('.tileModal').each(function(){
-      var $this = $(this);
-      var mc = new Hammer(this);
-      mc.on("press", function() {
-          modal.style.display = "block";
-          var header = tileGetAtributeByName($this.parent(),"tileDescription");
-          $(".modalHeader").text(header);
-      });
-  });
-
-  function tileGetAtributeByName(obj, desc){
-    return obj.find("." + desc).text();
-  }
+*/    
+  $(".slider").each(function() {
+    slider = new hx.Slider(this, {max:100});
+    slider.on('change', function(value){
+        console.log("test");
+        console.log(Math.round(value));
+         });
+});
 
 
+  $('select').formSelect();
+  $('.timepicker').timepicker();
 
   $('.graphModul').each(function(){
     var chartXaxis = ['1.1.2019','1.2.2019','1.3.2019','1.4.2019', ];
     var chartData = [12,10,-1,9]
     var header = this.getAttribute("data-header");
   
-
     //var ctx = document.getElementById('myChart');
     var ctx = $(this).children();
     var myChart = new Chart(ctx, {
@@ -99,11 +80,100 @@ $(document).ready(function(){
 
 
 
+/**
+ * 
+ *  Kód pro zpracovávání inputů ze stránky
+ * 
+ */
 
+  // CheckBox; 
+  $('input[type="checkbox"]').click(function(){
+    var IDofObject = $(this).parent().parent().parent().attr("data-id");
 
-  $('.hx.Slider').on('change', function(e){
-    console.log("aslkdmsjdn");
+    if($(this).prop("checked") == true){
+      var stateOfObject = "checked";
+      printToConsole(IDofObject, stateOfObject);
+    }
+    else if($(this).prop("checked") == false){
+      var stateOfObject = "unchecked";
+      printToConsole(IDofObject, stateOfObject);
+    }
   });
+
+
+  $('.tileToggle').each(function(){
+      var $this = $(this);
+      var mc = new Hammer(this);
+      mc.on("tap", function() {
+          $this.parent().toggleClass('tileActive');
+      });
+  });
+
+  $('.tileModal').each(function(){
+    var $this = $(this);
+    var mc = new Hammer(this);
+    mc.on("press", function() {
+      modal.style.display = "block";
+      var header = tileGetAtributeByName($this.parent(),"tileDescription");
+      $(".modalHeader").text(header);
+    });
+  });
+  
+  function printToConsole(objID, state){
+    console.log(objID + " is " + state);
+  }
+
+  function tileGetAtributeByName(obj, desc){
+    return obj.find("." + desc).text();
+  }
+
+  function sendRequst(params)
+  {
+    
+  }
+
+
+  /**
+   * 
+   *  Genreování modalového okna
+   * 
+   */  
+
+    var modal = document.getElementById("tile-Modal");
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // Close the modal when close button is pressed
+    window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
+ 
+  // Initializace Swiperu 
+  var swiper = new Swiper('.swiper-container', {
+    pagination: { el: '.swiper-pagination'},
+    threshold: '10',
+    //allowTouchMove: false,
+    //simulateTouch: false,
+    //touchStartPreventDefault: true,
+    //noSwiping: true,
+    //noSwipingClass = 'swiper-no-swiping'
+  });  
+
+
+
+
+
+
+
+
+  
 
 
 
